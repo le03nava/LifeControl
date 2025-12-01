c0f51a27ccb6374adc59441f73b78958128cdc1a
le03nava <leo3nava@gmail.com>
2025-12-01 17:05:14 -0600

    Gateway
    Gateway

--- api-gateway/src/main/java/com/lifecontrol/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/lifecontrol/gateway/config/SecurityConfig.java
@@ -1,40 +1,44 @@
 package com.lifecontrol.gateway.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.List;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
 
-    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
+  private final String[] freeResourceUrls = { "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+      "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus" };
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(freeResourceUrls)
-                        .permitAll()
-                        .anyRequest().authenticated())
-                //.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
-    }
-/*
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.applyPermitDefaultValues();
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-    */
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity.authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(freeResourceUrls)
+        .permitAll()
+        .anyRequest().authenticated())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        // .cors(Customizer.withDefaults())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .csrf(csrf -> csrf.disable())
+        .build();
+  }
+
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.applyPermitDefaultValues();
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE",
+        "OPTIONS", "HEAD"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
 }
--- api-gateway/src/main/java/com/lifecontrol/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/lifecontrol/gateway/routes/Routes.java
@@ -16,68 +16,71 @@ import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouter
 @Configuration
 public class Routes {
 
-    @Bean
-    public RouterFunction<ServerResponse> productServiceRoute(){
-        return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.GET("/api/product"),HandlerFunctions.http("http://localhost:8080"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> productServiceRoute() {
+    return GatewayRouterFunctions.route("product_service")
+        .route(RequestPredicates.path("/api/product/**"), HandlerFunctions.http("http://localhost:8080"))
+        .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+            URI.create("forward:/fallbackRoute")))
+        .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
-        return GatewayRouterFunctions.route("product_service_swagger")
-                .route(RequestPredicates.GET("/aggregate/product-service/v3/api-docs"),HandlerFunctions.http("http://localhost:8080"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .filter(FilterFunctions.setPath("/api-docs"))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+    return GatewayRouterFunctions.route("product_service_swagger")
+        .route(RequestPredicates.GET("/aggregate/product-service/v3/api-docs"),
+            HandlerFunctions.http("http://localhost:8080"))
+        .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
+            URI.create("forward:/fallbackRoute")))
+        .filter(FilterFunctions.setPath("/api-docs"))
+        .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> orderServiceRoute(){
-        return GatewayRouterFunctions.route("order_service")
-                .route(RequestPredicates.GET("/api/order"),HandlerFunctions.http("http://localhost:8080"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> orderServiceRoute() {
+    return GatewayRouterFunctions.route("order_service")
+        .route(RequestPredicates.GET("/api/order"), HandlerFunctions.http("http://localhost:8080"))
+        .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
+            URI.create("forward:/fallbackRoute")))
+        .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
-        return GatewayRouterFunctions.route("order_service_swagger")
-                .route(RequestPredicates.GET("/aggregate/order-service/v3/api-docs"),HandlerFunctions.http("http://localhost:8081"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceSwaggerCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .filter(FilterFunctions.setPath("/api-docs"))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+    return GatewayRouterFunctions.route("order_service_swagger")
+        .route(RequestPredicates.GET("/aggregate/order-service/v3/api-docs"),
+            HandlerFunctions.http("http://localhost:8081"))
+        .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceSwaggerCircuitBreaker",
+            URI.create("forward:/fallbackRoute")))
+        .filter(FilterFunctions.setPath("/api-docs"))
+        .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> inventoryServiceRoute(){
-        return GatewayRouterFunctions.route("inventory_service")
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .route(RequestPredicates.GET("/api/inventory"),HandlerFunctions.http("http://localhost:8080"))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> inventoryServiceRoute() {
+    return GatewayRouterFunctions.route("inventory_service")
+        .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
+            URI.create("forward:/fallbackRoute")))
+        .route(RequestPredicates.GET("/api/inventory"), HandlerFunctions.http("http://localhost:8080"))
+        .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
-        return GatewayRouterFunctions.route("inventory_service_swagger")
-                .route(RequestPredicates.GET("/aggregate/inventory-service/v3/api-docs"),HandlerFunctions.http("http://localhost:8082"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceSwaggerCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .filter(FilterFunctions.setPath("/api-docs"))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+    return GatewayRouterFunctions.route("inventory_service_swagger")
+        .route(RequestPredicates.GET("/aggregate/inventory-service/v3/api-docs"),
+            HandlerFunctions.http("http://localhost:8082"))
+        .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceSwaggerCircuitBreaker",
+            URI.create("forward:/fallbackRoute")))
+        .filter(FilterFunctions.setPath("/api-docs"))
+        .build();
+  }
 
-    @Bean
-    public RouterFunction<ServerResponse> fallbackRoute() {
-        return route("fallbackRoute")
-                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
-                        .body("Service Unavailable, please try again later"))
-                .build();
-    }
+  @Bean
+  public RouterFunction<ServerResponse> fallbackRoute() {
+    return route("fallbackRoute")
+        .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+            .body("Service Unavailable, please try again later"))
+        .build();
+  }
 }
